a58d1a6c9a4d7429b05dff13566f42121595f2b6
Sebastien Belanger-Nantel <arbiter@mainframe.mainframe>
2022-11-23 22:40:41 +0100

    install update

--- install.sh
+++ install.sh
@@ -6,14 +6,20 @@ exec 1>log.out 2>&1
 
 # INITIAL UPDATES and CORE PACKAGE 
 apt update && apt upgrade -f -y --force-yes
-apt install openssh-server git
+apt install -y openssh-server git
 sleep 5
 systemctl status sshd
 systemctl enable --now sshd
 
 # USER and GROUPS
 id -u arbiter &>/dev/null || useradd -m arbiter
-useradd -m vpn
+id -u vpn &>/dev/null || useradd -m vpn
+groupadd -f -g 1003 archive
+echo "Set password for arbiter"
+passwd arbiter
+echo "Set password for vpn"
+passwd vpn
+
 WORK_HOME=/home/arbiter
 cd $WORK_HOME
 
@@ -32,13 +38,11 @@ docker run hello-world
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
+# Clone the stacks and folder
+su -c 'git -C ~ clone https://github.com/g0rth0r/portainer-stacks.git' arbiter
 
 # OTHER PACKAGES
-apt install samba ffmpeg lvm2 smartmontools rsync rsnapshot
-mkdir -p $WORK_HOME/media-playback
-cd $WORK_HOME/media-playback; mkdir config tv movies
-cd $WORK_HOME
-apt install openvpn -y
+apt install -y samba ffmpeg lvm2 smartmontools rsync rsnapshot openvpn
 
 # Install for YT-DLP
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
@@ -48,8 +52,6 @@ apt install smartmontools
 
 # TELEGRAPH GRAPHANA INFLIXDB STACK
 # https://www.linuxserver.io/blog/2017-11-25-how-to-monitor-your-server-using-grafana-influxdb-and-telegraf
-mkdir -p $WORK_HOME/tgi-stack/appdata/influxdb
-mkdir -p $WORK_HOME/tgi-stack/appdata/grafana
 chown arbiter:root -R $WORK_HOME/tgi-stack/appdata/grafana
 chmod 775 -R $WORK_HOME/tgi-stack/appdata/grafana
 
